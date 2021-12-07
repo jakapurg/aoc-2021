0a5147fbd36b60ab4c685ecb238cac9da1db1467
Jaka Purg <purg.jaka@gmail.com>
2021-12-07 09:11:07 +0100

    Day 7

---
+++ solutions/7.1/aoc_7.1.go
@@ -0,0 +1,41 @@
+package main
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+	"math"
+)
+
+func main(){
+	file,_ := os.Open("reports.txt")
+	defer file.Close()
+	scanner:= bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var text[] string
+	for scanner.Scan(){
+		text = append(text,scanner.Text())
+	}
+	s:=strings.Split(text[0],",")
+	var pos = make([]int,len(s))
+	for i,val := range s{
+		pos[i],_ = strconv.Atoi(val)
+	}
+	fuel:= 0
+	smallestF:=math.MaxInt
+	mostOptimateI:=0
+	for x:=0;x<len(pos);x++{
+		fuel=0
+		for y:=0;y<len(pos);y++{
+			fuel+=int(math.Abs(float64(pos[y]-pos[x])))
+		}
+		if(fuel<smallestF){
+			smallestF=fuel
+			mostOptimateI=x
+		}
+	}
+	fmt.Println(smallestF,mostOptimateI)
+	
+
+}
---
+++ solutions/7.1/go.mod
@@ -0,0 +1,3 @@
+module aoc_7.1
+
+go 1.17
