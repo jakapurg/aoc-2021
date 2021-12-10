09e34004ac7c666c83e90dc7b9dcee7c8b5f3406
Jaka Purg <purg.jaka@gmail.com>
2021-12-10 23:32:28 +0100

    Template source file

--- template/aoc_day.solution.go
+++ template/aoc_day.solution.go
@@ -14,15 +14,4 @@ func main(){
 	for scanner.Scan(){
 		text = append(text,scanner.Text())
 	}
-	increaseCount:=0
-	prevVal,_:=strconv.Atoi(text[0])
-	var tmpVal int =0 
-	for _, val := range text[1:] {
-		tmpVal,_=strconv.Atoi(val)
-		if(prevVal<tmpVal){
-			increaseCount++
-		}
-		prevVal=tmpVal
-	}
-	fmt.Println(increaseCount)
 }
